0a2b1027295f2d61e8d9802bc97639d39f0ae4d4
Daniel Depaoli <daniel.depaoli@xpeppers.com>
2016-05-18 15:30:35 +0200

    Initial version of instances_table

---
+++ instances_table.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import json
+import sys
+import boto3
+from datetime import datetime
+from json import JSONDecoder
+from json import JSONEncoder
+
+class DateTimeDecoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kargs):
+        JSONDecoder.__init__(self, object_hook=self.dict_to_object,
+                             *args, **kargs)
+    
+    def dict_to_object(self, d): 
+        if '__type__' not in d:
+            return d
+
+        type = d.pop('__type__')
+        try:
+            dateobj = datetime(**d)
+            return dateobj
+        except:
+            d['__type__'] = type
+            return d
+
+class DateTimeEncoder(JSONEncoder):
+    """ Instead of letting the default encoder convert datetime to string,
+        convert datetime objects into a dict, which can be decoded by the
+        DateTimeDecoder
+    """
+        
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return {
+                '__type__' : 'datetime',
+                'year' : obj.year,
+                'month' : obj.month,
+                'day' : obj.day,
+                'hour' : obj.hour,
+                'minute' : obj.minute,
+                'second' : obj.second,
+                'microsecond' : obj.microsecond,
+            }   
+        else:
+            return JSONEncoder.default(self, obj)
+
+def get_value_from_key(key, dict):
+    if key in dict:
+        return dict[key]
+    return ""
+
+def get_name_from_tag(tag_list):
+    for tag in tag_list:
+        if tag["Key"] == "Name":
+            return tag["Value"]
+    return ""
+
+def main(profile="default"):
+    #js = json.loads(open("/tmp/prova.json", "r").read())
+    armani=boto3.session.Session(profile_name=profile)
+    ec2=armani.resource('ec2')
+
+    client=armani.client('ec2')
+    #js = json.loads(client.describe_instances(), object_hook=json_serial)
+    js = json.loads(json.dumps(client.describe_instances(),cls=DateTimeEncoder), cls=DateTimeDecoder)
+    for reservation in js["Reservations"]:
+        for instance in reservation["Instances"]:
+            print get_name_from_tag(get_value_from_key("Tags", instance)) + "\t" + \
+            get_value_from_key("InstanceId", instance) + "\t" + \
+            get_value_from_key("InstanceType", instance) + "\t" + \
+            get_value_from_key("AvailabilityZone", instance["Placement"]) + "\t" + \
+            get_value_from_key("Name", instance["State"]) + "\t" + \
+            get_value_from_key("PublicDnsName", instance) + "\t" + \
+            get_value_from_key("PublicIpAddress", instance) + "\t" + \
+            get_value_from_key("PrivateIpAddress", instance) + "\t" + \
+            get_value_from_key("KeyName", instance)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(profile=sys.argv[1])
+    else:
+        main(profile="default")
+
